0eeaef4f7a547eb0edef0f9dc5bd5d03f7ad38a7
IlhmRgn <117716197+IlhamRichie@users.noreply.github.com>
2026-09-01 23:12:55 +0700

    feat: remove deprecated incident model and related views/controllers

--- lib/modules/live_feed/controllers/live_feed_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class LiveFeedController extends GetxController {
-  final RxInt gridCount = 4.obs; // Default 4 cameras
-
-  void changeGrid(int count) {
-    gridCount.value = count;
-  }
-}
